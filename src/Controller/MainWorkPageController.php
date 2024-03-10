6641dfd83bfce0776f3fa0f1ee47c9b24e9e8833
wixxxez <vinewood02@gmail.com>
2024-03-10 05:19:15 +0200

    Create news Entity

--- src/Controller/MainWorkPageController.php
+++ src/Controller/MainWorkPageController.php
@@ -6,11 +6,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\News;
 
 class MainWorkPageController extends AbstractController
 {
     #[Route('/home', name: 'app_main_work_page')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
 
@@ -19,9 +21,33 @@ class MainWorkPageController extends AbstractController
             return $this->redirectToRoute('login'); // Or handle it differently
         }
 
+        $repository = $entityManager->getRepository(News::class);
+
+        // Query to fetch the entity with type "Admin Message" and the highest ID along with the "Text" parameter
+        $adminMessage = $repository->createQueryBuilder('n')
+            ->select('n.Text')
+            ->where('n.Type = :type')
+            ->setParameter('type', 'Admin Message')
+            ->orderBy('n.id', 'DESC')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        // Query to fetch the entity with type "Events" and the highest ID along with the "Text" parameter
+        $events = $repository->createQueryBuilder('n')
+            ->select('n.Text')
+            ->where('n.Type = :type')
+            ->setParameter('type', 'Events')
+            ->orderBy('n.id', 'DESC')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        $news  = ['admin'=> $adminMessage, 'events' => $events];
         return $this->render('main_work_page/index.html.twig', [
             'controller_name' => 'MainWorkPageController',
             'user' => $user,
+            'news' =>  $news
         ]);
     }
 }
